c61537257d80d6437b6db7a96bba39c02cb1a198
Michael Young <michaeljy@gmail.com>
2013-12-30 18:23:07 -0800

    first commit

---
+++ Pine Chapter 02/calc.rb
@@ -0,0 +1,9 @@
+# Pine Chapter 2
+
+puts 365*24  # Hours in a year
+puts 10*365*24*60 # Minutes in a decade
+puts 28*365*24*60*60 # Age in seconds
+
+# Our dear author's age
+# 1,025 million seconds old = how many years?
+puts 1025000000 / 60 / 60 / 24 / 365
